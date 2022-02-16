27eacbbf2b2bf0c10eb6744e41f7b87eb1ea30b9
dongkeke <dongkeke@zgmmtuan.com>
2022-02-16 17:12:16 +0800

    11

--- 我的创作/股票/技术投资/SEAP交易系统/交易分析与计划/2022-01/每股收益大于0.5-主板.files/js.js
+++ 我的创作/股票/技术投资/SEAP交易系统/交易分析与计划/2022-01/每股收益大于0.5-主板.files/js.js
@@ -1,8 +1,9 @@
-var c_lTabs=2;
+var c_lTabs=3;
 var c_rgszSh=new Array(c_lTabs);
 c_rgszSh[0]="技术面分析";
 c_rgszSh[1]="基本面分析";
-var g_iShCur=0;
+c_rgszSh[2]="Sheet1";
+var g_iShCur=2;
 var g_rglTabX=new Array(c_lTabs+1);
 var g_clrs=new Array(8);
 g_clrs[0]="window";
