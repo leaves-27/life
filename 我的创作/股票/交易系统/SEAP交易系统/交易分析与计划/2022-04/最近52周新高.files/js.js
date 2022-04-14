f2e70dd1c8c76fa68197a7f77a1b5cab8656644e
dongkeke <dongkeke@zgmmtuan.com>
2022-04-14 08:03:24 +0800

    11

--- 我的创作/股票/交易系统/SEAP交易系统/交易分析与计划/2022-04/最近52周新高.files/js.js
+++ 我的创作/股票/交易系统/SEAP交易系统/交易分析与计划/2022-04/最近52周新高.files/js.js
@@ -1,8 +1,11 @@
-var c_lTabs=2;
+var c_lTabs=5;
 var c_rgszSh=new Array(c_lTabs);
 c_rgszSh[0]="最近52周新高";
 c_rgszSh[1]="Sheet1";
-var g_iShCur=1;
+c_rgszSh[2]="Sheet2";
+c_rgszSh[3]="Sheet3";
+c_rgszSh[4]="Sheet4";
+var g_iShCur=2;
 var g_rglTabX=new Array(c_lTabs+1);
 var g_clrs=new Array(8);
 g_clrs[0]="window";
